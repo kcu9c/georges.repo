30a9b608138cf41b7e217b4ce0149e745b7498a2
kcu9c <gbmillion@bucs.fsw.edu>
2016-02-25 14:55:16 +0000

    completed example

--- ch_5/examples/jscript/app.js
+++ ch_5/examples/jscript/app.js
@@ -1,25 +1,23 @@
 var main = function () {
     "use strict";
     $.getJSON("cards/aceOfSpades.json", function (card) {
-        // create an element to hold the card
-        var $cardParagraph = $("<p>");
-        // create the 
+        // create object & add field then append string
+	var $cardParagraph = $("<p>");
         $cardParagraph.text(card.rank + " of " + card.suit);
-        // append the card paragraph to main
+        // put object to main
         $("main").append($cardParagraph);
     });
     $.getJSON("cards/hand.json", function (hand) {
         var $list = $("<ul>");
-        // hand is an array, so we can iterate over it
-        hand.forEach(function (card) {
-            // create a list item to hold the card
-            // and append it to the list
-            var $card = $("<li>");
+        // hand is an array, cycle threw
+	hand.forEach(function (card) {
+            // copy from hand to list
+       	    var $card = $("<li>");
             $card.text(card.rank + " of " + card.suit);
             $list.append($card);
         });
-        // append the list to main
-        $("main").append($list);
+        // put list array to main
+	$("main").append($list);
     });
-};
+}; // display
 $(document).ready(main);
